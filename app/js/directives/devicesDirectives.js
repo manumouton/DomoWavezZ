db8f32d6ac78b9f6b4c7d323442b21b4d861f919
manumouton <manumouton@gmail.com>
2015-09-15 10:08:13 +0200

    Managing templates for devices

---
+++ app/js/directives/devicesDirectives.js
@@ -0,0 +1,50 @@
+domoWaveZApp.directive('deviceItem', function ($compile, TemplatesServices) {
+    var getTemplate = function (devicesTemplates, deviceType) {
+        var template = '';
+
+        switch (deviceType) {
+            case deviceTypesData.BATTERY.id:
+                template = devicesTemplates.battery;
+                break;
+            case deviceTypesData.DOORLOCK.id:
+                template = devicesTemplates.doorlock;
+                break;
+            case deviceTypesData.THERMOSTAT.id:
+                template = devicesTemplates.thermostat;
+                break;
+            case deviceTypesData.SWITCH_BINARY.id:
+                template = devicesTemplates.switchbinary;
+                break;
+            case deviceTypesData.SWITCH_MULTILEVEL.id:
+                template = devicesTemplates.switchmultilevel;
+                break;
+            case deviceTypesData.SENSOR_BINARY.id:
+                template = devicesTemplates.sensorbinary;
+                break;
+            case deviceTypesData.SENSOR_MULTILEVEL.id:
+                template = devicesTemplates.sensormultilevel;
+                break;
+            case deviceTypesData.SWITCH_TOGGLE.id:
+                template = devicesTemplates.switchtoggle;
+                break;
+        }
+
+        return template;
+    };
+
+    var linker = function (scope, element, attrs) {
+        TemplatesServices.devicesTemplates().then(function (response) {
+            var devicesTemplates = response.data;
+            element.html(getTemplate(devicesTemplates, scope.$parent.device.deviceType));
+            $compile(element.contents())(scope);
+        });
+    };
+
+    return {
+        restrict: 'E',
+        link: linker,
+        scope: {
+            content: '='
+        }
+    };
+});
--- app/js/services/services.js
+++ app/js/services/services.js
@@ -1,8 +1,11 @@
 "use strict";
 
-domoWaveZApp.factory("Services", function ($http) {
-    var API_URI = '/ZAutomation/api/v1/';
+/**
+ * Razberry API service
+ */
+domoWaveZApp.constant('API_URI', '/ZAutomation/api/v1/');
 
+domoWaveZApp.factory("Services", function ($http, API_URI) {
     return {
         devices: function () {
             return $http.get(API_URI + 'devices');
@@ -26,5 +29,17 @@ domoWaveZApp.factory("Services", function ($http) {
             return $http.get(API_URI + 'modules');
         }
     };
+});
 
+/**
+ * Templates service providing different items templates stored in json file
+ */
+domoWaveZApp.constant('DATA_URI', 'data/');
+
+domoWaveZApp.factory("TemplatesServices", function ($http, DATA_URI) {
+    return {
+        devicesTemplates: function () {
+            return $http.get(DATA_URI + 'devicesTemplates.json');
+        }
+    };
 });
